cc402a6652e8cf7f5bb40e6d58b3699f8c50a004
Yuxuan Wang <wangyuxuanbilly@gmail.com>
2025-08-05 15:59:39 +0800

    qwen3 support

--- native_sparse_attention/model/toy_nsa_llama.py
+++ native_sparse_attention/model/toy_nsa_llama.py
@@ -42,14 +42,7 @@ class ToyNSALlamaConfig:
         }
     )
     # nsa config
-    compress_type: str = "weightedpool"
-    kernel_size: int = 32
-    kernel_stride: int = 16
-    block_size: int = 64
-    topk: int = 16
-    init_blocks: int = 1
-    local_blocks: int = 2
-    window_size: int = 512
+     
 
 
 @dataclass
